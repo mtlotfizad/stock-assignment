a5b6123ee91069057315666fe7cd08d48c72516b
mohsen <mtlotfizad@gmail.com>
2019-02-10 19:22:28 +0330

    spring boot skeleton

---
+++ src/main/java/com/lotfizad/stockassignment/StockAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.lotfizad.stockassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAssignmentApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/lotfizad/stockassignment/StockAssignmentApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lotfizad.stockassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockAssignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
