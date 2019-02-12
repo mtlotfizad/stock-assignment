58cfa62a36649a22c839eb5da9f4aaef445b5b23
mohsen <mtlotfizad@gmail.com>
2019-02-12 12:00:54 +0330

    add validations

--- src/main/java/com/lotfizad/stockassignment/api/viewModel/CreateStockViewModel.java
+++ src/main/java/com/lotfizad/stockassignment/api/viewModel/CreateStockViewModel.java
@@ -1,8 +1,12 @@
 package com.lotfizad.stockassignment.api.viewModel;
 
+import javax.validation.constraints.NotNull;
+
 public class CreateStockViewModel {
 
+    @NotNull
     private String name;
+    @NotNull
     private Double price;
 
     public String getName() {
